009df57cbe1a1ba5539d4d5f686e1343df30bdf1
Insaf <safinsaft@gmail.com>
2020-12-10 16:30:54 +0300

    OSU

---
+++ Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SA_Client
+{
+    class Program
+    {
+        
+        static async Task Main(string[] args)
+        {
+            OSU a = new OSU();
+            //await a.CreateUser("Insaf", "aaaaaaa@ssss.ru", "prettypassword");
+            //User newUser = await a.CreateUser("Vitaliy", "aaa@ssss.ru", "prettypassword");
+            Token b = await a.GetToken("Insaf", "aaaaaaa@ssss.ru", "prettypassword");
+            Console.WriteLine(b.access);
+            string accessToken = b.access;
+            List <Profile> pr = await a.SearchUser(accessToken,"Insaf");
+            Profile profile = await a.SubmitPlay(accessToken, "5", "true", "204", "1");
+            Console.WriteLine(1);
+            Console.WriteLine(profile.hours_played);
+            Console.WriteLine(2);
+            profile = await a.AddFriend(accessToken, "Vitaliy");
+            profile = await a.MyProfile(accessToken);
+            profile = await a.UpdateProfile(accessToken, "Inno", "", "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png");
+            List<Play> plays = await a.MyPlays(accessToken);
+            
+
+        }
+    }
+    
+    public class OSU
+    {
+        string URL = "http://mywarmplace.tk:8000/";
+
+        public OSU() {
+        }
+
+        public OSU(string url)
+        {
+            URL = url;
+        }
+
+        static readonly HttpClient client = new HttpClient();
+        
+        public async Task <Token> GetToken(string username, string email, string password)
+        {
+            try
+            {
+                Person person = new Person(username, email, password); 
+                var json = JsonConvert.SerializeObject(person);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+                var url = URL + "auth/jwt/create/";
+                HttpResponseMessage response = await client.PostAsync(url, data);
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(responseBody);
+                return token;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+            return null;
+        }
+
+        public async Task <User> CreateUser(string username, string email, string password)
+        {
+            try
+            {
+                Person person = new Person(username, email, password); 
+                var json = JsonConvert.SerializeObject(person);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+                var url = URL + "auth/users/";
+                HttpResponseMessage response = await client.PostAsync(url, data);
+                string responseBody = await response.Content.ReadAsStringAsync();
+                User user = JsonConvert.DeserializeObject<User>(responseBody);
+                return user;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+            return null;
+        }
+        
+        public async Task <List<Profile>> SearchUser(string accessToken, string search = "")
+        {
+            try
+            {
+                var url = URL + "all-profiles" + "?search=" + search;
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                HttpResponseMessage response = await client.GetAsync(url);
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+                List<Profile> profiles = JsonConvert.DeserializeObject<List<Profile>>(responseBody);
+                return profiles;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            return null;
+        }
+        
+        
+        public async Task <Profile> SubmitPlay(string accessToken, string length, string passed, string score, string mapid)
+        {
+            try
+            {
+                var url = URL + "play" + 
+                    "?length="+length +
+                    "&passed="+passed+
+                    "&score="+score+
+                    "&mapid="+mapid;
+                
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                HttpResponseMessage response = await client.PutAsync(url,null);
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Profile profile = JsonConvert.DeserializeObject<Profile>(responseBody);
+                return profile;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            return null;
+        }
+        
+        
+        public async Task <Profile> AddFriend(string accessToken, string name)
+        {
+            try
+            {
+                var url = URL + "add-friend" +
+                          "?name=" + name;
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                HttpResponseMessage response = await client.PutAsync(url,null);
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Profile profile = JsonConvert.DeserializeObject<Profile>(responseBody);
+                return profile;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            return null;
+        }
+        
+        public async Task <Profile> MyProfile(string accessToken)
+        {
+            try
+            {
+                var url = URL + "myprofile";
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                HttpResponseMessage response = await client.GetAsync(url);
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Profile profile = JsonConvert.DeserializeObject<Profile>(responseBody);
+                return profile;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            return null;
+        }
+
+        public async Task <Profile> UpdateProfile(string accessToken, string location, string description, 
+            string profilePictureUrl)
+        {
+            try
+            {
+                var url = URL + "myprofile";
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken); ; 
+                Content content = new Content(location, description, profilePictureUrl); 
+                var json = JsonConvert.SerializeObject(content);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(url, data);
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Profile profile = JsonConvert.DeserializeObject<Profile>(responseBody);
+                return profile;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            return null;
+        }
+        
+        public async Task <List<Play>> MyPlays(string accessToken)
+        {
+            try
+            {
+                var url = URL + "myplays";
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                HttpResponseMessage response = await client.GetAsync(url);
+                string responseBody = await response.Content.ReadAsStringAsync();
+                List<Play> plays = JsonConvert.DeserializeObject<List<Play>>(responseBody);
+                return plays;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+            return null;
+        }
+    }
+}
---
+++ Serial.cs
@@ -0,0 +1,109 @@
+using System.Collections.Generic;
+
+namespace SA_Client
+{
+    public class Person
+    {
+        public string username;
+        public string email;
+        public string password;
+
+        public Person(string username, string email, string password)
+        {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
+    }
+
+    public class User
+    {
+        public string email;
+        public string username;
+        public string id;
+
+        public User(string email, string username, string id)
+        {
+            this.email = email;
+            this.username = username;
+            this.id = id;
+        }
+    }
+
+    public class Play
+    {
+        public string id;
+        public string date;
+        public string score;
+        public string mapid;
+        public string user;
+
+        public Play(string id, string date, string score, string mapid, string user)
+        {
+            this.id = id;
+            this.date = date;
+            this.score = score;
+            this.mapid = mapid;
+            this.user = user;
+        }
+    }
+    
+    public class Content
+    {
+        public string location;
+        public string description;
+        public string profile_picture_url;
+
+        public Content(string location, string description, string profile_pictur_url)
+        {
+            this.location = location;
+            this.description = description;
+            this.profile_picture_url = profile_pictur_url;
+        }
+    }
+
+    public class Profile
+    {
+        public string id;
+        public string user;
+        public string description;
+        public string location;
+        public string date_joined;
+        public string hours_played;
+        public string successful_map_plays;
+        public string map_plays;
+        public string total_score;
+        public string profile_picture_url;
+        public List<string> friends;
+
+        public Profile(string id, string user, string description, string location,
+            string dateJoined, string hoursPlayed, string successfulMapPlays,
+            string mapPlays, string totalScore, string profilePictureUrl, 
+            List<string> friends)
+        {
+            this.id = id;
+            this.user = user;
+            this.description = description;
+            this.location = location;
+            this.date_joined = dateJoined;
+            this.hours_played = hoursPlayed;
+            this.successful_map_plays = successfulMapPlays;
+            this.map_plays = mapPlays;
+            this.total_score = totalScore;
+            this.profile_picture_url = profilePictureUrl;
+            this.friends = friends;
+        }
+    }
+
+    public class Token
+    {
+        public string refresh;
+        public string access;
+
+        public Token(string refresh, string access)
+        {
+            this.refresh = refresh;
+            this.access = access;
+        }
+    }
+}
